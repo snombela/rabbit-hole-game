96f87d2a74116cd79800550fe019255703c97083
Sofía <sofianombela@gmail.com>
2019-01-21 16:27:57 +0100

    Move player

--- player.js
+++ player.js
@@ -4,40 +4,43 @@ function Player (game){
     this.x = this.game.canvas.width / 2;
     this.y = this.game.canvas.height / 2;
     this.radius = 20;
-    this.speedX = 0; //Si quiero que vaya más rápido le pongo números más altos a x e y; //sumar 0 todo el rato y cuando pulses una tecla modifica la speed x o y.
+    this.speedX = 0; 
     this.speedY = 0;
     this.setListeners();
 }
 
 Player.prototype.move = function() {
-    this.y += this.speedY;
-    this.x += this.speedX;
+    if (this.y >= this.radius && this.y <= this.game.canvas.height - this.radius){
+        this.y += this.speedY;
+    } else if (this.y < this.radius){
+        this.y = this.radius;
+    } else {
+        this.y = this.game.canvas.height - this.radius;
+    }
+
+    if(this.x >= this.radius && this.x <= this.game.canvas.width - this.radius) {
+        this.x += this.speedX;
+    } else if (this.x < this.radius){
+        this.x = this.radius;
+    } else {
+        this.x = this.game.canvas.width - this.radius;
+    }
 }
 
 Player.prototype.moveUp = function(){
-    if (this.y - this.radius - this.speedY >= 0) {
-        this.speedY -= 4;
-    }
+        this.speedY -= 0.3;
 }
 
 Player.prototype.moveDown = function(){
-    height = this.game.canvas.height
-    if (this.y + this.radius + this.speedY <= height) {
-        this.speedY += 4;
-    }
+        this.speedY += 0.3;
 }
 
 Player.prototype.moveLeft = function(){
-    if (this.x - this.radius - this.speedX >= 0) {
-        this.speedX -= 4;
-    }
+        this.speedX -= 0.3;
 }
 
 Player.prototype.moveRight = function(){
-    width = this.game.canvas.width;
-    if (this.x + this.radius + this.speedX <= width) {
-        this.speedX += 4;
-    }
+        this.speedX += 0.3;
 } 
 
 Player.prototype.draw = function (){
@@ -53,40 +56,19 @@ Player.prototype.draw = function (){
 Player.prototype.setListeners = function (){
     document.onkeydown = function(e) {
         switch (e.keyCode) {
-          case 38: 
-            this.moveUp();
-            break;
-          case 40: 
-            this.moveDown();
-            break;
-          case 37: 
-            this.moveLeft();
-            break;
-          case 39: 
-            this.moveRight();
-            break;
-        }
-        
+          case 38: this.moveUp(); break;
+          case 40: this.moveDown(); break;
+          case 37: this.moveLeft(); break;
+          case 39: this.moveRight(); break;
+        } 
     }.bind(this);  
     
     document.onkeyup = function (e){
         switch (e.keyCode) {
-            case 38: 
-            this.speedY = 0;
-            
-            break;
-            case 40: 
-            this.speedY = 0;
-            
-            break;
-            case 37: 
-            this.speedX = 0;
-            
-            break;
-            case 39: 
-            this.speedX = 0;
-            
-            break;
+            case 38: this.speedY = 0; break;
+            case 40: this.speedY = 0; break;
+            case 37: this.speedX = 0; break;
+            case 39: this.speedX = 0; break;
         }
     }.bind(this);
 }
