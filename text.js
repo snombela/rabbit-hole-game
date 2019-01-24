9731f0e22b33d594b4ce0d4d371e7fdd59b22436
Sofía <sofianombela@gmail.com>
2019-01-24 20:42:31 +0100

    fix text align

--- text.js
+++ text.js
@@ -5,6 +5,7 @@ function Text (game){
 Text.prototype.draw = function (){
     this.game.ctx.font = '540 small-caps 35px Courier New';
     this.game.ctx.fillStyle='white';
+    this.game.ctx.textAlign = "left";
     this.game.ctx.fillText('Round: ' + this.game.level, 16, 35);
   
 }
